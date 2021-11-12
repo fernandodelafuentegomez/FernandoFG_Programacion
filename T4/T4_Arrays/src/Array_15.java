e0e00ec2294bca8718b267e70e56460b0fbf9f35
fernandodelafuentegomez <fernandodelafuentegomez@gmail.com>
2021-11-12 12:08:52 +0100

    12/11/2021

---
+++ T4/T4_Arrays/src/Array_15.java
@@ -0,0 +1,26 @@
+public class Array_15 {
+
+    public static void main(String[] args) {
+
+        int par = 0;
+        int Impar = 0;
+        int[] numero = new int[15];
+
+        for (int i = 0; i < 15; i++) {
+            numero[i] = (int) (Math.random() * 51);
+            System.out.println(numero[i]);
+
+            if (numero[i] % 2 == 0) {
+                par++;
+            } else {
+                Impar++;
+            }
+
+        }
+        System.out.println("hay " + par + "numero pares");
+        System.out.println("hay " + Impar + "numero Impares");
+
+
+    }
+}
+}

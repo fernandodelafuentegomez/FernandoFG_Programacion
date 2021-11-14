7052b271a4a5ffe3e4c59f6b1b62d1ba232f3bb6
fernandodelafuentegomez <fernandodelafuentegomez@gmail.com>
2021-11-14 18:22:20 +0100

    14/10/2021

--- T4/T4_Arrays/src/Array_15.java
+++ T4/T4_Arrays/src/Array_15.java
@@ -23,4 +23,3 @@ public class Array_15 {
 
     }
 }
-}
--- T4/T4_Arrays/src/Arrays.java
+++ T4/T4_Arrays/src/Arrays.java
@@ -1,20 +1,28 @@
 public class Arrays {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        //array de numeros, esto es meter en una variable varios datos
+        int[] numeros = new int[10];
+        int[] numerosDos = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
 
-            int par = 0;
-            int [] numero = new int[15];
+        boolean[] aciertos = new boolean[5];
+        String[] palabras = new String[3];
+        double[] decimales = {1.25, 2.69, 7.58};
 
-            for (int i = 0; i<15; i++) {
-                numero[i] = (int)(Math.random()*51);
-                System.out.println(numero[i]);
+        //ver numero de elementos que tiene
+        int longitudArray = numerosDos.length;
+        System.out.println("La longitus de numerosDos es: " + longitudArray);
 
-                if(numero[i] % 2 == 0) {
-                    par++;
-                }
-            }
+        //Para sacar una posicion concreta del Arrays
+        System.out.printf("La posicion 7 del Arrays es: " + numerosDos[7]);
 
-            System.out.println("hay " + par + "numero pares");
+        //Sacar todas las posiciones
 
+        for (int i = 0; i < numerosDos.length; i++){
+            System.out.println(numerosDos[i]);
+            System.out.println("Hola Mundo");
         }
+
     }
+
+}
